abfe694f264aff00805c79268d59ce320e645c45
Muhammad Rafly Juliano <raflyjuliano62@gmail.com>
2024-07-02 01:11:00 -0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Toggle class 'active' on the navbar menu
+const navbarNav = document.querySelector(".navbar-nav");
+
+// When the hamburger menu is clicked
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// klik di luar sidebar untuk menghilangkan nav
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
